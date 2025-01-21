4b625c1e9ea64528797a1da3cb7aa6c5737e8296
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2025-01-21 11:26:12 +0100

    fått till shared layout med dosplay flex med navigering i en sidebar och huduvinnehållet till höger

--- obj/Debug/net9.0/m1_1.AssemblyInfo.cs
+++ obj/Debug/net9.0/m1_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("m1_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb58344b9936545139d914eb6be44d8276db2adb")]
 [assembly: System.Reflection.AssemblyProductAttribute("m1_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("m1_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
